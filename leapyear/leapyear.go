d2bd3f1106aa57f72abdc170b0349126c2d48115
Luis Moral Guerrero <luis.moral@gmail.com>
2018-08-23 17:34:47 +0200

    Added leapyear divisible by 4 tests.

---
+++ leapyear/leapyear.go
@@ -0,0 +1,22 @@
+package leapyear
+
+import (
+	"flag"
+	"fmt"
+)
+
+// LeapYear returns if the year is a leap year
+func LeapYear(year int) bool {
+	if year%4 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	year := flag.Int("year", 0, "Year")
+	flag.Parse()
+
+	fmt.Println(LeapYear(*year))
+}
---
+++ leapyear/leapyear_test.go
@@ -0,0 +1,15 @@
+package leapyear
+
+import "testing"
+
+func TestLeapYearWhenDivisibleBy4(t *testing.T) {
+	if !LeapYear(1996) {
+		t.Fail()
+	}
+}
+
+func TestLeapYearWhenNotDivisibleBy4(t *testing.T) {
+	if LeapYear(1997) {
+		t.Fail()
+	}
+}
